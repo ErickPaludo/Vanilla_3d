7d3b74126c70851a6eca8958cfdcc2c55970ca0d
Érick Augusto Paludo <erickpaludoyt@gmail.com>
2024-05-19 11:28:36 -0300

    dd

---
+++ sinc/Capture/GravarDados.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capture
+{
+    public class GravarDados
+    {
+        public GravarDados()
+        {
+        }
+
+        public GravarDados(int rua, int predio, int la)
+        {
+            this.rua = rua;
+            this.predio = predio;
+            this.la = la;
+        }
+
+        public int rua {  get; set; }
+        public int predio { get; set; }
+        public int la { get; set; }
+        public void GravarJson(GravarDados dados)
+        {
+            string novoJson = JsonConvert.SerializeObject(dados);
+
+            // Anexa os novos dados ao arquivo JSON, criando o arquivo se ele não existir
+            File.AppendAllText(@"C:\Users\erick\source\Repos\Capture\Capture\obj\Debug\net8.0\dados.json", novoJson + Environment.NewLine);
+        }
+
+        public void ExcluirArquivo()
+        {
+            // Verifica se o arquivo existe antes de tentar excluí-lo
+            if (File.Exists(@"C:\Users\erick\source\Repos\Capture\Capture\obj\Debug\net8.0\dados.json"))
+            {
+                File.Delete((@"C:\Users\erick\source\Repos\Capture\Capture\obj\Debug\net8.0\dados.json"));
+            }
+        }
+    }
+}
---
+++ sinc/Capture/Program.cs
@@ -0,0 +1,19 @@
+﻿using Capture;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System;
+using System.Data;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+            int y = 2;
+            Console.WriteLine($"{x} e {y}");
+        }
+    }
+}
